2431c94ad8da16d042e033f6ec054481bfdbce70
IvanTomaev <ivam.tomaev.01@gmail.com>
2021-12-05 15:39:52 +0300

    изменяемый порт хоста

--- localhost_test.py
+++ localhost_test.py
@@ -7,16 +7,18 @@ window=''
 def update():
     global panel
     img = Image.open(r'C:\Users\Computer\Desktop\image.jpg')
+    img = img.resize((640, 480), Image.ANTIALIAS)
     v = ImageTk.PhotoImage(img)
     panel.config(image=v)
     panel.image=v
     img.close()
-    window.after(1000, update)
+    window.after(500, update)
 def draw():
     global panel
     global window
     window = tk.Tk()
     img = Image.open(r'C:\Users\Computer\Desktop\image.jpg')
+    img = img.resize((640, 480), Image.ANTIALIAS)
     v = ImageTk.PhotoImage(img)
     panel = tk.Label(image=v)
     panel.pack()
@@ -24,9 +26,9 @@ def draw():
     window.after(1000,update)
     window.mainloop()
 
-def prog():
+def prog(PORT):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(('', 9090))
+    sock.bind(('', PORT))
     print('binded')
     sock.listen(1)
     conn, addr = sock.accept()
@@ -50,10 +52,28 @@ def prog():
                 n=not n
         else:
             file.write(data)
+def send():
+    global window2
+    global port_entry
+    global port_entry
+    PORT=int(port_entry.get())
+    window2.destroy()
+    prog(PORT)
 
-prog()
-
-
-
+def init():
+    global port_entry
+    global window2
+    window2 = tk.Tk()
+    label = tk.Label(master=window2,text='Введите порт')
+    port_entry = tk.Entry(master=window2,fg="black", bg="white", width=50)
+    button = tk.Button(master=window2,text="Запустить сервер", command=send)
+    label.pack()
+    port_entry.pack()
+    button.pack()
+    window2.mainloop()
+port_entry=''
+window2=''
+thread=threading.Thread(target=init)
+thread.start()
 
 
